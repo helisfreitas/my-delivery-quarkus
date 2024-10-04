bbffe939f6fbddb61e6a3d497fe2ff55304ffbb1
Helis Freitas <dfhelis@gmail.com>
2024-10-04 21:26:20 +0000

    feat: add Integration Test for Restaurant Api using DBunit with DBRider CDI and Approval Test for json asserts
    Note: It was necessary remove stax dependency from DBRider Cdi maven dependency  to correct  work

---
+++ my-delivery-registration/src/test/java/dev/helis/registration/RegistrationTestLifecycleManager.java
@@ -0,0 +1,34 @@
+package dev.helis.registration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class RegistrationTestLifecycleManager implements QuarkusTestResourceLifecycleManager {
+
+    public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:12.0");
+
+    @Override
+    public Map<String, String> start() {
+        POSTGRES.start();
+
+        Map<String, String> proprieties = new HashMap<String, String>();
+
+        proprieties.put("quarkus.datasource.jdbc.url", POSTGRES.getJdbcUrl());
+        proprieties.put("quarkus.datasource.username", POSTGRES.getUsername());
+        proprieties.put("quarkus.datasource.password", POSTGRES.getPassword());
+        
+        return proprieties;
+    }
+
+    @Override
+    public void stop() {
+       if(POSTGRES != null && POSTGRES.isRunning()) {
+           POSTGRES.stop();
+       }
+    }
+    
+}
