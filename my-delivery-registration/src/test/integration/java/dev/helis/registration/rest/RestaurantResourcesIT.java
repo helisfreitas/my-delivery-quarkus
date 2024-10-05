c5be18cd95912e5673757e1d4a66f396b7592aa9
Helis Freitas <dfhelis@gmail.com>
2024-10-05 16:07:06 +0000

    feat: add test case for custom validation

--- my-delivery-registration/src/test/integration/java/dev/helis/registration/rest/RestaurantResourcesIT.java
+++ my-delivery-registration/src/test/integration/java/dev/helis/registration/rest/RestaurantResourcesIT.java
@@ -11,6 +11,7 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.cdi.api.DBRider;
 
 import dev.helis.helper.RegistrationTestLifecycleManager;
+import dev.helis.helper.annotation.ErrorTest;
 import dev.helis.helper.annotation.IntegrationRestTest;
 import dev.helis.helper.annotation.SuccessTest;
 import io.quarkus.test.common.QuarkusTestResource;
@@ -37,5 +38,21 @@ class RestaurantResourcesIT {
                 .extract().asString();
 
         JsonApprovals.verifyJson(response);
+    }
+
+    @ErrorTest
+    void shouldShowErrorOnCreateDishesFromRestaurantInvalidName() {
+
+        String json = "{\"name\": \"New Flavor123\", \"owner\": \"Manuel\", \"location\": {\"longitude\": \"33º00'00.0E\", \"latitude\": \"33º00'00.0N\"}}";
+
+        String response = given()
+                .when().contentType("application/json").body(json)
+                .post(ResourcePaths.RESTAURANTS)
+                .then()
+                .statusCode(400)
+                .extract().response().body().asString();
+
+        JsonApprovals.verifyJson(response);
+
     }
 }
